1db4b19c7211f065f56b518a32a1d89cbe16f8f7
mahdizouari <mahdizouari96@gmail.com>
2024-08-03 14:19:57 +0100

    §§

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -1,14 +1,23 @@
-@extends('layouts.base')
-
-@section('title', 'Fripsakyetna')
-
-@section('content')
-
-
-<section>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>product detail</title>
+</head>
+<body>
+<section >
+    <center>
     <div class="container">
         aaa
+        <div>
+        <a href="/" class="btn btn-primary"  >retourner</a>
+        <form action="{{route('product.detail',$product->id)}}">
+            <button class="flex-c-m stext-104 cl0 size-105 bg3 bor2 hov-btn2 p-lr-19 trans-04">welcome</button>
+        </form>
+        </div>
     </div>
+    </center>
+</section>   
+</body>
+</html>
 
-</section>
-@endsection         
